aa6a0b02e79688e42a46f82193802fdc8e3f4fd4
xianxuehui <40551921@ncepu.edu.cn>
2017-04-22 20:29:10 +0800

    format

--- src/main/java/org/seckill/dao/SeckillDao.java
+++ src/main/java/org/seckill/dao/SeckillDao.java
@@ -12,15 +12,32 @@ import java.util.Map;
  */
 public interface SeckillDao {
 
-    //减库存
-
+    /**
+     * 减库存
+     * @param seckillId
+     * @param killTime
+     * @return
+     */
     int reduceNumber(@Param("seckillId")long seckillId,@Param("killTime")Date killTime);
-    //根据Id查询秒杀对象
+
+    /**
+     * 根据Id查询秒杀对象
+     * @param seckillId
+     * @return
+     */
     Seckill queryById(long seckillId);
-    //根据偏移量查询秒杀列表
+
+    /**
+     * 根据偏移量查询秒杀列表
+     * @param offset
+     * @param limit
+     * @return
+     */
     List<Seckill> queryAll(@Param("offset")int offset,@Param("limit")int limit);
+
     /**
      * 使用存储过程执行秒杀
+     * @param paramMap
      */
     void killByProcedure(Map<String,Object> paramMap);
 }
